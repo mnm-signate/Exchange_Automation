1ff614dc5a9dc59214100b83aaa222252b022a66
mnm_analytics <mnm.signate@gmail.com>
2018-10-28 18:17:16 +0900

    modify line.py
    ã³ã¼ãç­ç¸® æ½è±¡å

--- src/models/line.py
+++ src/models/line.py
@@ -1,11 +1,7 @@
 # coding=utf-8
 import requests
 
-def execute(body):
-    line_notify_token = '自分のline_notifyのtoken'
-    line_notify_api = 'https://notify-api.line.me/api/notify'
-    message = body
-
-    payload = {'message': message}
-    headers = {'Authorization': 'Bearer ' + line_notify_token}
-    line_notify = requests.post(line_notify_api, data=payload, headers=headers)
+def execute(body, token, api):
+    payload = {'message': body}
+    headers = {'Authorization': 'Bearer ' + token}
+    line_notify = requests.post(api, data=payload, headers=headers)
